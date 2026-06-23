9d2064607b0eb171bfa6d80ba5e6f61ecb5edc2f
Nalla Chandana <chandana193@gmail.com>
2026-06-23 22:25:58 +0530

    add the local storage

--- certificate-generator/src/app/services/student.service.ts
+++ certificate-generator/src/app/services/student.service.ts
@@ -5,12 +5,29 @@ import { Injectable } from '@angular/core';
 })
 export class StudentService {
 
-   private students: any[] = [];
+    private students: any[] = [];
 
   private selectedStudent: any;
 
+  constructor() {
+
+    const storedStudents = localStorage.getItem('students');
+
+    if (storedStudents) {
+      this.students = JSON.parse(storedStudents);
+    }
+
+  }
+
   setStudents(data: any[]) {
+
     this.students = data;
+
+    localStorage.setItem(
+      'students',
+      JSON.stringify(this.students)
+    );
+
   }
 
   getStudents() {
